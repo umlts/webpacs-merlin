f4a0608f2bdd80bfb319086f916878d8e2e50e94
hammondjn <jessica@mobiusconsortium.org>
2012-07-12 10:45:43 -0500

    Deleted the MobileCat stuff, since that doesn't belong in MERLIN

--- mobilecat-1.1/static/relogin.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function () {
-    var d = new Date();
-    window.parent.$("#loginfooter").load(BASEURL + "loginfooter?" + d.getTime());
-});
--- mobilecat-1.1/static/resizer.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function () {
-    //window.parent.$("iframe").css("height", $("body").height()); 
-});
