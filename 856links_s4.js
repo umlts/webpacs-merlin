5ae55c5cff1c23831405590903425729adeebb9b
Megan Durham <megan@mobiusconsortium.org>
2014-05-21 14:56:14 -0500

    change js to display urls that belong to all

--- 856links_s4.js
+++ 856links_s4.js
@@ -2,8 +2,8 @@ function hideLinks(){
     
     var stringsToHide = new Array ();
     stringsToHide[1] = new Array ("mul.missouri", "EW3DM6ND8C");
-    stringsToHide[4] = new Array ("umkc", "PC8GA3QQ6A", "UMKC");
-    stringsToHide[5] = new Array ("mst", "UM9MH3KU7S", "MS&T");
+    stringsToHide[4] = new Array ("umkc", "UMKC", "PC8GA3QQ6A");
+    stringsToHide[5] = new Array ("mst", "MST", "UM9MH3KU7S");
     stringsToHide[6] = new Array ("umsl", "UMSL", "FN2VR5VT8L");
     
 
@@ -31,4 +31,38 @@ function hideLinks(){
     
     $(document).ready(function () {
 	hideLinks();
+});
+    function showLinks(){
+    
+    var stringsToShow = new Array ();
+    stringsToShow[1] = new Array ("Freely available online", "MU, MST, UMKC, UMSL online access");
+    stringsToShow[4] = new Array ("Freely available online", "MU, MST, UMKC, UMSL online access");
+    stringsToShow[5] = new Array ("Freely available online", "MU, MST, UMKC, UMSL online access");
+    stringsToShow[6] = new Array ("Freely available online", "MU, MST, UMKC, UMSL online access");
+    
+
+
+    var scopeDropdown = $("#searchscope").val();
+    
+    delete stringsToShow[scopeDropdown];
+	
+    if ($("table").is('.bibLinks')){
+	for (var k in stringsToShow) {
+	    for (var l in stringsToShow[k]) {
+		$(".bibLinks a").each(function(index, element) {
+		    if($(element).parent().html().indexOf(stringsToShow[k][l]) > -1) {
+			$(element).parent().parent().show()
+		    }    
+		});
+	    }
+	}
+	if ($(".bibLinks tr[style*=none]").length < $(".bibLinks a").length){
+	    $(".bibLinks").show();       
+	}
+    }  
+}
+    
+    
+    $(document).ready(function () {
+	showLinks();
 });
